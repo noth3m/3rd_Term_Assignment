8761211a20f9574c0189a5e5cd5d4e174b203a18
noth3m <hamdmoinsid@gmail.com>
2024-02-18 21:37:25 +0530

    commit message hi

---
+++ c.py
@@ -0,0 +1,54 @@
+'''uncomment a program to test.'''
+## 1 ##
+# a = input("Enter Your Name: ")
+# print(f"Hi {a}!")
+## 2 ##
+# num1 = int(input("Enter First Number: "))
+# num2 = int(input("Enter Second Number: "))
+# print(f"The sum of the two numbers is {num1 + num2}!!")
+## 3 ##
+# age = int(input("What is your age? "))
+# birth_year = 2024 - age
+# print(f'You were born in {birth_year}')
+## 4 ##
+# rad = int(input("Enter radius of the circle: "))
+# print(f'Area of the circle is {3.14*(rad**2)}')
+## 5 ##
+# oper = int(input("Enter operation you want to perform: "))
+# num_1 = int(input("Enter First Number: "))
+# num_2 = int(input("Enter Second Number: "))
+# if oper == "-":
+#     print(f"The Difference of the two numbers is {num_1 - num_2}")
+# elif oper == "+":
+#     print(f"The sum of the two numbers is {num_1 + num_2}")
+# elif oper == "*":
+#     print(f"The product of the two numbers is {num_1 * num_2}")
+# elif oper == "/":
+#     print(f"The quotient of the two numbers is {num_1 // num_2}")
+# else:
+#     print("Input Error")
+## 6 ##
+# a = input("What is your favourite color?? ")
+# print(a)
+## 7 ##
+# Celsius = int(input("Enter your temperature in Celsius: "))
+# print(f'The temperature in Farenheit is {Celsius*9/5 + 32}')
+## 8 ##
+# eoo = int(input("Enter your number: "))
+# if eoo%2 == 0: 
+#     print(f'{eoo} is even')
+# else:
+#     print(f'{eoo} is odd')
+## 9 ##
+# str = input("Enter string: ")
+# print(f"string length is {len(str) }")
+## 10 ##
+# age = int(input("What is your age? "))
+# if age > 17 :
+#     print("You are eligible to vote!!")
+# else:
+#     print("You are not eligible to vote")                
+## 11 ##
+# str = input("Enter string: ")
+# vowel_count = str.count('a') + str.count('e') + str.count('i') + str.count('o') + str.count('u') 
+# print(f'Number of vowels is {vowel_count}')
