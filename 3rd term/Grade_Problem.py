d0509321817c9d0341687239a7d9b76eee580aa2
noth3m <hamdmoinsid@gmail.com>
2024-02-26 00:51:14 +0530

    .

--- 3rd term/Grade_Problem.py
+++ 3rd term/Grade_Problem.py
@@ -11,5 +11,22 @@ class student:
     Grade = input_info()
     Theory = Grade[0]
     Viva = Grade[1]
+    def remark():
+        good = ['Good Job , Keep it up','Wow! , You killed it!!','']
     def check_pass_or_fail(self):
-        if 
+        if self.Theory > 35:
+            check = 'pass'
+            if self.Viva == 0:
+                check = 'absent'
+        else:
+            check = 'fail'
+        return check
+    state = check_pass_or_fail()
+    def average(self):
+        average = self.Theory + self.Viva
+        average = average/2
+        return average
+    average = int(average())
+    abs_marks = average()
+# still being worked on 
+            
